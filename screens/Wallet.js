118e7a2bc4f6b4c397d0a2a4e2dca4ab40167f47
ShermanNovak <sk33t0722@gmail.com>
2022-10-09 18:25:09 +0800

    garden

--- screens/Wallet.js
+++ screens/Wallet.js
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Body from '../components/Body.js'
 
 const Wallet = () => {
   return (
-    <View>
-      <Text>Wallet</Text>
-    </View>
+    <Body title='My Wallet'>
+        
+    </Body>
   )
 }
 
